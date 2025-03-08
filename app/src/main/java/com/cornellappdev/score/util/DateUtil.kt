6b64bcb3d0a61409cd917826723f9a90d8e46477
amjiao <amyjiaowang@gmail.com>
2025-03-08 13:11:59 -0500

    Date functions refactor

---
+++ app/src/main/java/com/cornellappdev/score/util/DateUtil.kt
@@ -0,0 +1,39 @@
+package com.cornellappdev.score.util
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converts date of form String "month-abbr day (day-of-week)" (for example, "Apr 29 (Tue)") to a LocalDate object
+ */
+
+fun formatDate(strDate: String): LocalDate? {
+    val subDate = strDate.substringBefore(" (")
+    val formatter = DateTimeFormatter.ofPattern("MMM d")
+
+    return try {
+        LocalDate.parse("$subDate ${LocalDate.now().year}", formatter) //assumes current year
+    } catch (e: Exception) {
+        null
+    }
+}
+
+/**
+ * Converts from format "#xxxxxx" to a valid hex, with alpha = 40. Ready to be passed into Color()
+ */
+fun formatColor(color: String): Int {
+    val alpha = (40 * 255 / 100)// Convert percent to hex (0-255)
+    val colorInt = Integer.parseInt(color.removePrefix("#"), 16)
+    return (alpha shl 24) or colorInt
+}
+
+/**
+ *  Takes in a LocalDate? object and returns it as a String of format "MM/DD/YYY"
+ */
+fun dateToString(date: LocalDate?): String {
+    if (date == null) {
+        return "--"
+    }
+    //Log.d("HomeViewModel", "formatedDate: ${date.month.value}/${date.dayOfMonth}/${date.year}")
+    return "${date.month.value}/${date.dayOfMonth}/${date.year}"
+}
