66fbd8093e2c90d0a59e729fb0fd72abfc4b9f2a
amjiao <amyjiaowang@gmail.com>
2025-03-09 16:23:41 -0400

    small fixes

--- app/src/main/java/com/cornellappdev/score/screen/HomeScreen.kt
+++ app/src/main/java/com/cornellappdev/score/screen/HomeScreen.kt
@@ -24,7 +24,6 @@ import com.cornellappdev.score.components.SportCard
 import com.cornellappdev.score.components.SportSelectorHeader
 import com.cornellappdev.score.components.UpcomingGamesCarousel
 import com.cornellappdev.score.model.ApiResponse
-import com.cornellappdev.score.theme.Style.heading1
 import com.cornellappdev.score.theme.Style.title
 import com.cornellappdev.score.viewmodel.HomeViewModel
 import kotlin.math.min
@@ -50,37 +49,7 @@ fun HomeScreen(
                 ) {
                     CircularProgressIndicator()
                 }
-            
-                //TODO: check - displaying the earliest three games
-                UpcomingGamesCarousel(
-                    uiState.upcomingGameList.subList(
-                        0,
-                        minOf(3, uiState.upcomingGameList.size)
-                    )
-                )
-                Column {
-                    Text(
-                        text = "Game Schedule",
-                        style = heading1,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 24.dp, vertical = 8.dp)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    SportSelectorHeader(
-                        sports = uiState.selectionList,
-                        selectedGender = uiState.selectedGender,
-                        selectedSport = uiState.sportSelect,
-                        onGenderSelected = {
-                            homeViewModel.onGenderSelected(it)
-                        },
-                        onSportSelected = {
-                            homeViewModel.onSportSelected(it)
-                        }
-                    )
-                }
             }
-
             is ApiResponse.Error -> {
                 //TODO: Add Error screen
                 Box(
@@ -132,9 +101,119 @@ fun HomeScreen(
     }
 }
 
-@Preview(showBackground = true)
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview
 @Composable
 private fun HomeScreenPreview() {
     HomeScreen()
 }
+
+
+//@Composable
+//fun HomeScreen(
+//    homeViewModel: HomeViewModel = hiltViewModel()
+//) {
+//    val uiState = homeViewModel.collectUiStateValue()
+//    val filteredGames = uiState.filteredGames
+//    val firstThreeGames = filteredGames.subList(0, min(filteredGames.size, 3)) //TODO: is this okay to be in the view?
+//
+//    Column(
+//        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Top),
+//        modifier = Modifier.statusBarsPadding()
+//    ) {
+//        when (uiState.loadedState) {
+//            is ApiResponse.Loading -> {
+//                //TODO: Add loading screen
+//                Box(
+//                    modifier = Modifier.fillMaxSize(),
+//                    contentAlignment = Alignment.Center
+//                ) {
+//                    CircularProgressIndicator()
+//                }
+//
+//                //TODO: check - displaying the earliest three games
+//                UpcomingGamesCarousel(
+//                    uiState.upcomingGameList.subList(
+//                        0,
+//                        minOf(3, uiState.upcomingGameList.size)
+//                    )
+//                )
+//                Column {
+//                    Text(
+//                        text = "Game Schedule",
+//                        style = heading1,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 24.dp, vertical = 8.dp)
+//                    )
+//                    Spacer(modifier = Modifier.height(8.dp))
+//                    SportSelectorHeader(
+//                        sports = uiState.selectionList,
+//                        selectedGender = uiState.selectedGender,
+//                        selectedSport = uiState.sportSelect,
+//                        onGenderSelected = {
+//                            homeViewModel.onGenderSelected(it)
+//                        },
+//                        onSportSelected = {
+//                            homeViewModel.onSportSelected(it)
+//                        }
+//                    )
+//                }
+//            }
+//
+//            is ApiResponse.Error -> {
+//                //TODO: Add Error screen
+//                Box(
+//                    modifier = Modifier.fillMaxSize(),
+//                    contentAlignment = Alignment.Center
+//                ) {
+//                    Text(
+//                        text = "Failed to load games. Please try again.",
+//                    )
+//                }
+//            }
+//
+//            is ApiResponse.Success -> {
+//                UpcomingGamesCarousel(firstThreeGames)
+//                Column {
+//                    Text(
+//                        text = "Game Schedule",
+//                        style = title,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(horizontal = 24.dp, vertical = 8.dp)
+//                    )
+//                    Spacer(modifier = Modifier.height(8.dp))
+//                    SportSelectorHeader(
+//                        sports = uiState.selectionList,
+//                        selectedGender = uiState.selectedGender,
+//                        selectedSport = uiState.sportSelect,
+//                        onGenderSelected = { homeViewModel.onGenderSelected(it) },
+//                        onSportSelected = { homeViewModel.onSportSelected(it) }
+//                    )
+//                    LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)) {
+//                        items(filteredGames.size) { page ->
+//                            val game = filteredGames[page]
+//                            SportCard(
+//                                teamLogo = game.teamLogo,
+//                                team = game.team,
+//                                date = game.dateString,
+//                                isLive = game.isLive,
+//                                genderIcon = painterResource(game.genderIcon),
+//                                sportIcon = painterResource(game.sportIcon),
+//                                location = game.location,
+//                                topCornerRound = true
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+//@Composable
+//private fun HomeScreenPreview() {
+//    HomeScreen()
+//}
