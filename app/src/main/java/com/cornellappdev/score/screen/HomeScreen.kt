e1ce60c3329b0b3349ded3b51605bce2950cc861
amjiao <amyjiaowang@gmail.com>
2025-03-17 10:48:29 -0400

    HomeContent abstraction

--- app/src/main/java/com/cornellappdev/score/screen/HomeScreen.kt
+++ app/src/main/java/com/cornellappdev/score/screen/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.cornellappdev.score.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,10 +26,10 @@ import com.cornellappdev.score.components.SportCard
 import com.cornellappdev.score.components.SportSelectorHeader
 import com.cornellappdev.score.components.UpcomingGamesCarousel
 import com.cornellappdev.score.model.ApiResponse
-import com.cornellappdev.score.model.GameCardData
 import com.cornellappdev.score.model.GenderDivision
 import com.cornellappdev.score.model.SportSelection
 import com.cornellappdev.score.theme.Style.title
+import com.cornellappdev.score.util.gameList
 import com.cornellappdev.score.viewmodel.HomeUiState
 import com.cornellappdev.score.viewmodel.HomeViewModel
 
@@ -51,6 +53,7 @@ fun HomeScreen(
                     CircularProgressIndicator()
                 }
             }
+
             is ApiResponse.Error -> {
                 //TODO: Add Error screen
                 Box(
@@ -79,7 +82,7 @@ private fun HomeContent(
     uiState: HomeUiState,
     onGenderSelected: (GenderDivision) -> Unit,
     onSportSelected: (SportSelection) -> Unit
-    ){
+) {
     UpcomingGamesCarousel(uiState.upcomingGames)
     Column {
         Text(
@@ -98,7 +101,7 @@ private fun HomeContent(
             onSportSelected = onSportSelected
         )
         LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)) {
-            items(uiState.filteredGames){
+            items(uiState.filteredGames) {
                 val game = it
                 SportCard(
                     teamLogo = game.teamLogo,
@@ -118,6 +121,21 @@ private fun HomeContent(
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    //TODO: parameters??
-    //HomeContent()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        HomeContent(
+            HomeUiState(
+                selectedGender = GenderDivision.ALL,
+                sportSelect = SportSelection.All,
+                selectionList = emptyList(),
+                loadedState = ApiResponse.Success(gameList)
+            ),
+            onGenderSelected = {},
+            onSportSelected = {}
+        )
+    }
 }
+
