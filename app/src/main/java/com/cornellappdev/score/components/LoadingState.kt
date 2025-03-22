8b226d58852c046a9525aa17e58d40a1ae8367d6
amjiao <amyjiaowang@gmail.com>
2025-03-22 12:24:33 -0400

    LoadingState set up

--- app/src/main/java/com/cornellappdev/score/components/LoadingState.kt
+++ app/src/main/java/com/cornellappdev/score/components/LoadingState.kt
@@ -1,18 +1,20 @@
 package com.cornellappdev.score.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cornellappdev.score.theme.GrayPrimary
 import com.cornellappdev.score.theme.GrayStroke
 import com.cornellappdev.score.theme.Style.heading1
 
 @Composable
-fun ErrorState(){
+fun LoadingState(){
     Column(
         modifier = Modifier.padding(24.dp)
     ){
@@ -22,5 +24,13 @@ fun ErrorState(){
             color = GrayStroke,
             modifier = Modifier.fillMaxWidth()
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        LoadingStateBox(12, 200.dp)
     }
+}
+
+@Preview
+@Composable
+private fun LoadingStatePreview(){
+    LoadingState()
 }
