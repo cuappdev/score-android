122f0217248bf88c953496e9195504d394e93ed4
Amy Wang <74884877+amjiao@users.noreply.github.com>
2025-04-14 18:27:14 -0400

    Address sport filter (issue #40) (#47)
    * Fix sport filter
    
    * Small readability fix

--- app/src/main/java/com/cornellappdev/score/viewmodel/PastGamesViewModel.kt
+++ app/src/main/java/com/cornellappdev/score/viewmodel/PastGamesViewModel.kt
@@ -39,7 +39,7 @@ class PastGamesViewModel @Inject constructor(
     PastGamesUiState(
         selectedGender = GenderDivision.ALL,
         sportSelect = SportSelection.All,
-        selectionList = Sport.getSportSelectionList(),
+        selectionList = Sport.getSportSelectionList(GenderDivision.ALL),
         loadedState = ApiResponse.Loading
     )
 ) {
@@ -70,7 +70,8 @@ class PastGamesViewModel @Inject constructor(
     fun onGenderSelected(gender: GenderDivision) {
         applyMutation {
             copy(
-                selectedGender = gender
+                selectedGender = gender,
+                selectionList = Sport.getSportSelectionList(gender)
             )
         }
     }
