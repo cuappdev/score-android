d33706815a61c018a184b9cd84f9e6f5628a3a13
zachseidner1 <zachary.seidner@gmail.com>
2025-04-14 20:12:49 -0400

    Ellipses

--- app/src/main/java/com/cornellappdev/score/components/ScoreBox.kt
+++ app/src/main/java/com/cornellappdev/score/components/ScoreBox.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cornellappdev.score.model.GameData
@@ -104,6 +105,7 @@ fun TeamScoreRow(teamScore: TeamScore, totalTextColor: Color) {
             modifier = Modifier.weight(1f),
             textAlign = TextAlign.Center,
             maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
 
         teamScore.scoresByPeriod.forEach { score ->
