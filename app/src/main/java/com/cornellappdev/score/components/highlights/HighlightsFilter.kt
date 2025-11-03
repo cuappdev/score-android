83c08e75134cbdb65a27e5aaa5964fb260f6c661
amjiao <amyjiaowang@gmail.com>
2025-11-03 11:18:45 -0500

    HighlightsFilter fixes

--- app/src/main/java/com/cornellappdev/score/components/highlights/HighlightsFilter.kt
+++ app/src/main/java/com/cornellappdev/score/components/highlights/HighlightsFilter.kt
@@ -1,54 +1,61 @@
 package com.cornellappdev.score.components.highlights
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults.outlinedButtonColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cornellappdev.score.model.Sport
-import com.cornellappdev.score.theme.GrayStroke
+import com.cornellappdev.score.theme.GrayLight
+import com.cornellappdev.score.theme.GrayPrimary
+import com.cornellappdev.score.theme.Stroke
 import com.cornellappdev.score.theme.Style.bodyNormal
+import com.cornellappdev.score.theme.White
 import com.cornellappdev.score.util.sportList
 
 @Composable
-fun HighlightsFilterButton(
+private fun HighlightsFilterButton(
     sport: Sport,
-    onFilterSelected: (Sport) -> Unit
+    onFilterSelected: (Sport) -> Unit,
+    isSelected: Boolean = false,
 ) {
-    Row(
+    OutlinedButton(
         modifier = Modifier
-            .height(32.dp)
-            .border(
-                width = 1.dp,
-                color = GrayStroke,
-                shape = RoundedCornerShape(100)
-            )
-            .background(color = Color.White, shape = RoundedCornerShape(100))
-            .padding(horizontal = 12.dp, vertical = 4.dp)
-            .clip(shape = RoundedCornerShape(100))
-            .clickable { onFilterSelected },
-        verticalAlignment = Alignment.CenterVertically,
+            .height(32.dp),
+        border = BorderStroke(width = 1.dp, color = Stroke),
+        onClick = { onFilterSelected(sport) },
+        shape = RoundedCornerShape(100.dp),
+        colors = outlinedButtonColors(
+            containerColor = if (isSelected) GrayLight else White,
+            contentColor = GrayPrimary
+        ),
+        contentPadding = PaddingValues(horizontal = 12.dp, vertical = 4.dp),
     ) {
-        Image(
+        Icon(
             painter = painterResource(sport.emptyIcon),
-            contentDescription = "sport Icon"
+            contentDescription = "sport Icon",
+            tint = Color.Unspecified
         )
         Spacer(modifier = Modifier.width(4.dp))
         Text(
@@ -60,30 +67,32 @@ fun HighlightsFilterButton(
 
 @Composable
 fun HighlightsFilterRow(
+    sportList: List<Sport>,
     onFilterSelected: (Sport) -> Unit,
 ) {
     LazyRow(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 24.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        items(sportList.size) { i ->
-            HighlightsFilterButton(sportList[i], { onFilterSelected(sportList[i]) })
-            Spacer(Modifier.width(12.dp))
+        item { Spacer(Modifier.width(12.dp)) }
+        items(sportList) { item ->
+            HighlightsFilterButton(item, onFilterSelected)
         }
+        item { Spacer(Modifier.width(12.dp)) }
     }
 }
 
 @Preview
 @Composable
 private fun HighlightsFilterButtonPreview() {
-    HighlightsFilterButton(Sport.BASEBALL, {})
+    var isSelected by remember { mutableStateOf(false) }
+    HighlightsFilterButton(Sport.BASEBALL, { isSelected = !isSelected }, isSelected = isSelected)
 }
 
 @Preview
 @Composable
 private fun HighlightsFilterRowPreview() {
-    HighlightsFilterRow({})
+    HighlightsFilterRow(sportList, {})
 }
