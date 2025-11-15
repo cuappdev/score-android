0f47c50a461b2daec51f5d04625b9ea8e5fbf6ef
amjiao <amyjiaowang@gmail.com>
2025-11-14 23:31:01 -0500

    Address nit

--- app/src/main/java/com/cornellappdev/score/nav/ScoreNavigationBar.kt
+++ app/src/main/java/com/cornellappdev/score/nav/ScoreNavigationBar.kt
@@ -1,19 +1,15 @@
 package com.cornellappdev.score.nav
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import com.cornellappdev.score.nav.root.ScoreScreens
 import com.cornellappdev.score.nav.root.tabs
@@ -28,45 +24,38 @@ fun ScoreNavigationBar(
     navBackStackEntry: NavBackStackEntry?,
     modifier: Modifier = Modifier,
 ) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth(),
-        color = Color.White,
-        shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.Transparent
     ) {
-        NavigationBar(
-            modifier = modifier,
-            containerColor = Color.Transparent
-        ) {
-            tabs.map { item ->
-                val isSelected = item.screen == navBackStackEntry?.toScreen()
+        tabs.map { item ->
+            val isSelected = item.screen == navBackStackEntry?.toScreen()
 
-                NavigationBarItem(
-                    selected = isSelected,
-                    onClick = { navigateToScreen(item.screen) },
-                    colors = NavigationBarItemDefaults.colors().copy(
-                        selectedIndicatorColor = Color.Transparent
-                    ),
-                    icon = {
-                        Icon(
-                            painter = painterResource(id = if (isSelected) item.selectedIcon else item.unselectedIcon),
-                            contentDescription = null,
-                            tint = Color.Unspecified
-                        )
-                    },
-                    label = {
-                        Text(
-                            text = item.label,
-                            style = bodyMedium,
-                            color = if (isSelected) {
-                                CrimsonPrimary
-                            } else {
-                                GrayPrimary
-                            }
-                        )
-                    }
-                )
-            }
+            NavigationBarItem(
+                selected = isSelected,
+                onClick = { navigateToScreen(item.screen) },
+                colors = NavigationBarItemDefaults.colors().copy(
+                    selectedIndicatorColor = Color.Transparent
+                ),
+                icon = {
+                    Icon(
+                        painter = painterResource(id = if (isSelected) item.selectedIcon else item.unselectedIcon),
+                        contentDescription = null,
+                        tint = Color.Unspecified
+                    )
+                },
+                label = {
+                    Text(
+                        text = item.label,
+                        style = bodyMedium,
+                        color = if (isSelected) {
+                            CrimsonPrimary
+                        } else {
+                            GrayPrimary
+                        }
+                    )
+                }
+            )
         }
     }
 }
