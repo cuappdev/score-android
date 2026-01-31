b439f225ffe20d5ba524f3b4fba362ac2403ac02
amjiao <amyjiaowang@gmail.com>
2026-01-31 17:06:47 -0500

    highlights models

--- app/src/main/java/com/cornellappdev/score/model/Highlights.kt
+++ app/src/main/java/com/cornellappdev/score/model/Highlights.kt
@@ -1,12 +1,16 @@
 package com.cornellappdev.score.model
 
+import com.cornellappdev.score.util.isValidSport
+import com.example.score.HighlightsQuery
+
 data class VideoHighlightData(
     val title: String,
     val thumbnailImageUrl: String,
     val videoUrl: String,
     val date: String,
-    val sport: Sport,
-    val gender: GenderDivision
+    val sport: Sport?,
+    val gender: GenderDivision?,
+    val duration: String?
 )
 
 data class ArticleHighlightData(
@@ -14,17 +18,59 @@ data class ArticleHighlightData(
     val imageUrl: String,
     val articleUrl: String,
     val date: String,
-    val sport: Sport
+    val sport: Sport?
 )
 
 sealed class HighlightData {
     abstract val title: String
+    abstract val date: String
+    abstract val sport: Sport?
 
     data class Video(val data: VideoHighlightData) : HighlightData() {
         override val title = data.title
+        override val date = data.date
+        override val sport = data.sport
     }
 
     data class Article(val data: ArticleHighlightData) : HighlightData() {
         override val title = data.title
+        override val date = data.date
+        override val sport = data.sport
     }
+}
+
+fun HighlightsQuery.YoutubeVideo.toHighlightData(): HighlightData? {
+    val sportName = sportsType ?: return null
+    if (!isValidSport(sportName)) return null
+
+    return HighlightData.Video(
+        VideoHighlightData(
+            title = title,
+            thumbnailImageUrl = thumbnail,
+            videoUrl = url,
+            date = publishedAt,
+            sport = Sport.fromDisplayName(sportName),
+            gender = if (title.contains("Men's")) {
+                GenderDivision.MALE
+            } else {
+                GenderDivision.FEMALE
+            },
+            duration = duration
+        )
+    )
+}
+
+fun HighlightsQuery.Article.toHighlightData(): HighlightData? {
+    val sportName = sportsType
+    if (!isValidSport(sportName)) return null
+
+    return HighlightData.Article(
+        ArticleHighlightData(
+            title = title,
+            imageUrl = image ?: return null,
+            articleUrl = url,
+            date = publishedAt,
+            sport = Sport.fromDisplayName(sportName)
+        )
+    )
 }
---
+++ app/src/main/java/com/cornellappdev/score/model/HighlightsRepository.kt
@@ -0,0 +1,61 @@
+package com.cornellappdev.score.model
+
+import android.util.Log
+import com.apollographql.apollo.ApolloClient
+import com.cornellappdev.score.util.isValidSport
+import com.example.score.HighlightsQuery
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import javax.inject.Inject
+import javax.inject.Singleton
+
+private const val TIMEOUT_TIME_MILLIS = 5000L
+private const val PAGE_LIMIT = 100
+private const val MAX_RETRIES = 3
+private const val PAGE_TIMEOUT_MILLIS = 3000L
+
+@Singleton
+class HighlightsRepository @Inject constructor(
+    private val apolloClient: ApolloClient,
+    private val appScope: CoroutineScope,
+) {
+    private val _highlightsFlow =
+        MutableStateFlow<ApiResponse<List<HighlightData>>>(ApiResponse.Loading)
+    val highlightsFlow = _highlightsFlow.asStateFlow()
+
+
+    /**
+     * Asynchronously fetches the list of games from the API. Once finished, will send down
+     * `upcomingGamesFlow` to be observed.
+     */
+    fun fetchHighlights() = appScope.launch {
+        _highlightsFlow.value = ApiResponse.Loading
+        try {
+            val result =
+                withTimeout(TIMEOUT_TIME_MILLIS) {
+                    apolloClient.query(HighlightsQuery()).execute().toResult()
+                }
+
+            if (result.isSuccess) {
+                val highlights = result.getOrNull()
+                val highlightsList =
+                    highlights?.articles.orEmpty().mapNotNull { it?.toHighlightData() } +
+                            highlights?.youtubeVideos.orEmpty().mapNotNull { it?.toHighlightData() }
+
+                _highlightsFlow.value =
+                    ApiResponse.Success(highlightsList)
+
+            } else {
+                _highlightsFlow.value = ApiResponse.Error
+            }
+
+        } catch (e: Exception) {
+            Log.e("HighlightsRepository", "Error fetching posts: ", e)
+            _highlightsFlow.value = ApiResponse.Error
+        }
+    }
+
+}
