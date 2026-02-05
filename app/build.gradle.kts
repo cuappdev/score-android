16ce4413e2f20ea672c769bf2c67296d75c4008e
amjiao <amyjiaowang@gmail.com>
2026-02-05 14:42:37 -0500

    change version code to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.cornellappdev.score"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
