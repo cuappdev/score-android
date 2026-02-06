ddd6f125f8c0a0999beeac04d6f0bb2c9922f819
amjiao <amyjiaowang@gmail.com>
2026-02-06 13:57:07 -0500

    Update version code to 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.cornellappdev.score"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
