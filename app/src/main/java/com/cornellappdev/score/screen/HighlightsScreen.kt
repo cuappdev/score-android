de8f4cd6172d41902d7b325495dbe06b8297f0e2
amjiao <amyjiaowang@gmail.com>
2026-04-22 11:12:51 -0400

    Address coderabbit comments

--- app/src/main/java/com/cornellappdev/score/screen/HighlightsScreen.kt
+++ app/src/main/java/com/cornellappdev/score/screen/HighlightsScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -83,6 +85,7 @@ private fun HighlightsScreenContent(
 ) {
     Column(
         modifier = Modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         Column(
             modifier = Modifier.padding(horizontal = 24.dp)
